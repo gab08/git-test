df0f9559c9312535e0d5be7bd731942d4b6d566e
gab26 <gab26@163.com>
2021-06-01 16:09:29 +0800

    master中test2提交

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("hello2");
         System.out.println("hot-fix");
         System.out.println("hot-fix2222");
-        System.out.println("mast test");
+        System.out.println("mast test2");
         System.out.println("hot-fix test");
     }
 }
