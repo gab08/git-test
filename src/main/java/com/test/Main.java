c75a86a471bdad5516da5c1baed7f136683a1507
gab26 <gab26@163.com>
2021-06-01 15:39:54 +0800

    hot-fix中第2次提交

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2222");
     }
 }
