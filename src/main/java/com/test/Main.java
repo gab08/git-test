d4a95b6d8013d88aa119f56677cf3189b5ee7225
gab26 <gab26@163.com>
2021-06-02 09:34:22 +0800

    push本地提交

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("hot-fix2222");
         System.out.println("mast test2");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
